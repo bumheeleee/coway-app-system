fee145561bfc2ecee85ff89cc677f98b423a8691
dlqjagml <dlqjagml6447@naver.com>
2022-11-12 18:36:41 +0900

    feat : item 검색 repository 구현 - 아이템이름과 카테고리로 검색 구현 완료 - test 완료

--- src/main/kotlin/assignment/cowaysystem/feature/order/repository/ItemRepository.kt
+++ src/main/kotlin/assignment/cowaysystem/feature/order/repository/ItemRepository.kt
@@ -1,7 +1,24 @@
 package assignment.cowaysystem.feature.order.repository
 
 import assignment.cowaysystem.feature.order.entity.item.Item
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ItemRepository: JpaRepository<Item, Long> {
+    @Query(
+            """
+                select i
+                from Item i
+                where (
+                    :searchKeyword is null or i.name like :searchKeyword or i.category.name = :searchKeyword
+                )
+            """
+    )
+    fun search(
+            pageable: Pageable,
+            @Param("searchKeyword") searchKeyword: String? = null
+    ): Page<Item>
 }
---
+++ src/test/kotlin/assignment/cowaysystem/feature/order/repository/ItemRepositoryTest.kt
@@ -0,0 +1,89 @@
+package assignment.cowaysystem.feature.order.repository
+
+import assignment.cowaysystem.feature.order.entity.Category
+import assignment.cowaysystem.feature.order.entity.item.AirCleaner
+import assignment.cowaysystem.feature.order.entity.item.Bidet
+import assignment.cowaysystem.feature.order.service.ItemService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.test.annotation.Rollback
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@SpringBootTest
+internal class ItemRepositoryTest {
+    @PersistenceContext
+    lateinit var em: EntityManager
+
+    @Autowired
+    lateinit var itemRepository: ItemRepository
+
+    @Autowired
+    lateinit var itemService: ItemService
+
+    @Autowired
+    lateinit var categoryRepository: CategoryRepository
+
+    @Test
+    @Transactional
+    fun searchTest() {
+        // given
+        var category1 = Category().also {
+            it.name = "비데"
+        }
+        var category2 = Category().also {
+            it.name = "매트리스"
+        }
+        var category3 = Category().also {
+            it.name = "정수기"
+        }
+        var category4 = Category().also {
+            it.name = "공기청정기"
+        }
+        categoryRepository.save(category1)
+        categoryRepository.save(category2)
+        categoryRepository.save(category3)
+        categoryRepository.save(category4)
+
+
+        var bidet = Bidet().also {
+            it.name = "a123"
+            it.category = category1
+        }
+        var bidet2 = Bidet().also {
+            it.name = "b123"
+            it.category = category1
+        }
+        var airCleaner1 = AirCleaner().also {
+            it.name = "air123"
+            it.category = category4
+        }
+        itemRepository.save(bidet)
+        itemRepository.save(bidet2)
+        itemRepository.save(airCleaner1)
+
+        val pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "name"))
+
+        // when
+        val bidetA = itemRepository.search(pageRequest,"a123")
+        val bidetB = itemRepository.search(pageRequest,"b123")
+        val bidets = itemRepository.search(pageRequest,"비데")
+
+        // then
+        assertEquals(bidetA.totalElements, 1)
+        assertEquals(bidetB.totalElements, 1)
+        assertEquals(bidets.totalElements, 2)
+
+        assertEquals(bidetA.content[0].name, "a123")
+        assertEquals(bidetB.content[0].name, "b123")
+        bidets.forEach {
+            println(it.name)
+        }
+
+    }
+}
