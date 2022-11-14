6d4994c0cb98406714f63e27dc849c05a14e3c75
dlqjagml <dlqjagml6447@naver.com>
2022-11-15 01:10:13 +0900

    feat : 카테고리 서비스 예외처리

--- src/main/kotlin/assignment/cowaysystem/feature/order/service/CategoryService.kt
+++ src/main/kotlin/assignment/cowaysystem/feature/order/service/CategoryService.kt
@@ -1,6 +1,7 @@
 package assignment.cowaysystem.feature.order.service
 
 import assignment.cowaysystem.common.exception.BadRequestException
+import assignment.cowaysystem.common.exception.NotFoundException
 import assignment.cowaysystem.feature.order.entity.Category
 import assignment.cowaysystem.feature.order.entity.item.Item
 import assignment.cowaysystem.feature.order.repository.CategoryRepository
@@ -19,7 +20,7 @@ class CategoryService(
 
     fun findItemByCategory(name: String): List<Item>{
         val findCategory = categoryRepository.findByName(name)
-                ?: throw BadRequestException("${name}은/는 존재하지 않는 카테고리입니다.")
+                ?: throw NotFoundException("${name}은/는 존재하지 않는 카테고리입니다.")
         return findCategory.items
     }
 }
