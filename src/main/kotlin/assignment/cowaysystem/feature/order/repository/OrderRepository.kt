b48df91f7767dd0b8431ea4dbd29882c6ee0c749
dlqjagml <dlqjagml6447@naver.com>
2022-11-15 00:54:18 +0900

    feat : findOrder Repository 추가 - test 완료

--- src/main/kotlin/assignment/cowaysystem/feature/order/repository/OrderRepository.kt
+++ src/main/kotlin/assignment/cowaysystem/feature/order/repository/OrderRepository.kt
@@ -2,6 +2,15 @@ package assignment.cowaysystem.feature.order.repository
 
 import assignment.cowaysystem.feature.order.entity.Order
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface OrderRepository: JpaRepository<Order, Long> {
+
+    @Query("""
+        select o
+        from Order o join o.member m
+        where m.loginId = :loginId
+    """)
+    fun findOrderByLoginId(@Param("loginId") loginId: String): List<Order>
 }
