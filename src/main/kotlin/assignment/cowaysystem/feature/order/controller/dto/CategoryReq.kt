1893a1d196f42a32c21be981f034d289084a24c6
dlqjagml <dlqjagml6447@naver.com>
2022-11-17 22:28:26 +0900

    fix : CategoryReq 수정 - 예외 형식에 맞춰 수정

--- src/main/kotlin/assignment/cowaysystem/feature/order/controller/dto/CategoryReq.kt
+++ src/main/kotlin/assignment/cowaysystem/feature/order/controller/dto/CategoryReq.kt
@@ -2,11 +2,9 @@ package assignment.cowaysystem.feature.order.controller.dto
 
 import assignment.cowaysystem.feature.order.entity.Category
 import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
 
 class CategoryReq(
-        @field:NotNull(message = "카테고리 이름은 NULL값이 들어올 수 없습니다.")
-        @field:NotBlank(message = "카테고리 이름은 공백값이 들어올 수 없습니다.")
+        @field:NotBlank(message = "name 입력형식이 올바르지 않습니다.")
         val name: String? = null
 ){
     fun toCategory(): Category{
