260a733cac491e5f44a91660b9ed188f4854b615
dlqjagml <dlqjagml6447@naver.com>
2022-11-17 22:14:14 +0900

    fix : OrderReq dto 수정 - count 범위 수정

--- src/main/kotlin/assignment/cowaysystem/feature/order/controller/dto/OrderReq.kt
+++ src/main/kotlin/assignment/cowaysystem/feature/order/controller/dto/OrderReq.kt
@@ -1,5 +1,6 @@
 package assignment.cowaysystem.feature.order.controller.dto
 
+import org.hibernate.validator.constraints.Range
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Pattern
 
@@ -10,7 +11,7 @@ class OrderReq(
         @field:NotBlank(message = "itemName 입력형식이 올바르지 않습니다.")
         val itemName: String? = null,
 
-        @field:NotBlank(message = "count 입력형식이 올바르지 않습니다.")
+        @field:Range(min = 1, max = 100)
         val count: Int = 0,
 
         @field:NotBlank(message = "color 입력형식이 올바르지 않습니다.")
