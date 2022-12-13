b38fcc87d05693706cebbbc8eab79942e36183ee
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-12-14 00:32:13 +0900

    fix : jwt security 설정 버그 해결 - compile 에러 해결 - 필요한 filter 추가

---
+++ src/main/kotlin/assignment/cowaysystem/config/CorsConfig.kt
@@ -0,0 +1,22 @@
+package assignment.cowaysystem.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter{
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true      // 내 서버가 응답을 할때 json을 자바스크립트에서 처리할 수 있게 할지를 결정
+        config.addAllowedOrigin("*")  // 모든 ip에 응답을 허용하겠다.
+        config.addAllowedHeader("*") // 모든 header에 응답을 허용하겠다.
+        config.addAllowedMethod("*")    // 모든 메서드의 요청을 허용하겠다.
+        source.registerCorsConfiguration("**", config)
+        return CorsFilter(source)
+    }
+}
--- src/main/kotlin/assignment/cowaysystem/config/SecurityConfig.kt
+++ src/main/kotlin/assignment/cowaysystem/config/SecurityConfig.kt
@@ -1,28 +1,45 @@
 package assignment.cowaysystem.config
 
+import lombok.RequiredArgsConstructor
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.filter.CorsFilter
 
-
+@Configuration
 @EnableWebSecurity
-class SecurityConfiguration: WebSecurityConfigurerAdapter() {
-
+@RequiredArgsConstructor
+class SecurityConfiguration(
+        private val corsFilter: CorsFilter
+): WebSecurityConfigurerAdapter() {
+    /**
+     * jwt를 사용하기 위해
+     * 1. formLogin 사용 x
+     * 2. httpBasic 사용 x
+     * 3. session 사용 x
+     */
     override fun configure(http: HttpSecurity) {
-
         http.csrf().disable()
-
         // 세션을 사용하지 않겠다.
         // form login 사용하지 않겠다.
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(corsFilter)   //모든 요청이 corsFilter를 타서 cors 정책으로부터 벗어날 수 있다.
                 .formLogin().disable()
                 .httpBasic().disable()
                 .authorizeRequests()
+                .antMatchers("/members/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/managers/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/admins/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
     }
 
     @Bean
